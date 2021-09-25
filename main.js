1e0567799b78b1d9c3cd545bc19c19adb045bf01
Patrick Penaranda <patrickjabonillo.26@gmail.com>
2021-09-25 10:29:05 +0800

    changing the layout

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function realdate() {
     let amPm = (hours < 12 ) ? "AM":"PM";
 
     hours = (hours > 12) ? hours-12: hours;
-    hours = ("0" + hours).slice(-1)
+    hours = ("0" + hours).slice(-2)
     minutes = ("0" + minutes).slice(-2)
     seconds = ("0"+seconds).slice(-2)
 
