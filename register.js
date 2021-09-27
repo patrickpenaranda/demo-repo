f64b5156496a7fbaeefb477b46e7bc10375d5063
Patrick Penaranda <patrickjabonillo.26@gmail.com>
2021-09-27 11:57:30 +0800

    dropdown functions

---
+++ register.js
@@ -0,0 +1,52 @@
+window.onload = async ()=>{
+    let area = await fetch('barangay.json').then((data)=>{return data.json()}).then((completedata)=> {
+        return Object.keys(completedata);
+    })
+    
+    let select_city =  document.getElementById("city");
+    let option_cities;
+    for(let i = 0; i < area.length; i++) {
+        option_cities = document.createElement("option");
+        option_cities.setAttribute("id","city")
+        option_cities.setAttribute("value",area[i])
+        option_cities.innerText = area[i];
+        select_city.appendChild(option_cities);
+    }
+
+    
+         select_city.addEventListener("click",async (e)=>{
+        let target = e.target;
+        let area =  document.getElementById("barangay");
+        for(let index = 0; index < area.length ;index++) {
+            console.log(area.options[index].text);
+        }
+        if(target.value !== ""){
+            let barangay = await fetch('barangay.json').then((data)=>{return data.json()}).then((completedata)=> {
+                return completedata[select_city.value];
+            })
+            
+            for(let i = 1; i<=100; i++) {
+                let areas_select = document.getElementById("barangay");
+                areas_select.remove(areas_select.options[i]);
+
+            }
+
+            for(let i = 0; i<barangay.length; i++) {
+                let areas_select = document.getElementById("barangay");
+                let area_option;
+                area_option= document.createElement("option");
+                area_option.setAttribute("id","barangays")
+                area_option.innerHTML = barangay[i]
+                areas_select.appendChild(area_option);
+            }
+        } else {
+            for(let i = 1; i<=100; i++) {
+                let areas_select = document.getElementById("barangay");
+                areas_select.remove(areas_select.options[i]);
+
+            }
+        }
+        
+    })
+}
+
