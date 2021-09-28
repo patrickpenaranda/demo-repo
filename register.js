cf31b41057a2041539aa605e73fa50b16df8457c
Patrick Penaranda <patrickjabonillo.26@gmail.com>
2021-09-28 11:37:34 +0800

    added choices button function and color

--- register.js
+++ register.js
@@ -33,10 +33,11 @@ window.onload = async ()=>{
             let areas_select = document.getElementById("barangay");
             let lngth = await areas_select.length;
             for(let i = 1; i<=lngth; i++) {
-                
                 areas_select.remove(areas_select.options[i]);
-
             }
+            let area_option = document.createElement("option");
+            area_option.innerHTML = "--select your barangay--"
+            areas_select.appendChild(area_option);
 
             for(let i = 0; i<barangay.length; i++) {
                 let areas_select = document.getElementById("barangay");
@@ -49,6 +50,8 @@ window.onload = async ()=>{
         }
         
     })
+    
+    //Profile change
     let file_name = document.getElementById("file-name");
     file_name.onchange = ()=> {
         let profile = document.getElementById("profile");
@@ -61,7 +64,7 @@ window.onload = async ()=>{
             fileReader.readAsDataURL(file[0]);
         }
     }
-
+    //Capslock values
     let last_input = document.getElementById("lname");
     last_input.addEventListener("keyup", (event)=> {
         event.target.value = event.target.value.toUpperCase(); 
@@ -74,6 +77,42 @@ window.onload = async ()=>{
     middle_input.addEventListener("keyup", (event)=> {
         event.target.value = event.target.value.toUpperCase(); 
         })
+
+    //click h1
+    let first_choices_div = document.getElementById("first-choices");
+    first_choices_div.addEventListener("click",changehElement)
+
+    let second_choices_div = document.getElementById("second-choices");
+    second_choices_div.addEventListener("click",changehElement)
+
+    let third_choices_div = document.getElementById("third-choices");
+    third_choices_div.addEventListener("click",changehElement)
+
+    let fourth_choices_div = document.getElementById("fourth-choices");
+    fourth_choices_div.addEventListener("click",changehElement)
+
+
+    function changehElement(element) {
+        let target = element.target;
+        for(let i = 0; i <= 2; i++) {
+            try {
+                if(target.parentElement.children[i].style.backgroundColor == "rgb(19, 134, 241)") {
+                    target.parentElement.children[i].style.backgroundColor = "white"
+                    target.parentElement.children[i].style.boxShadow = "0px 0px 5px rgba(110,110,110,.3)"
+                    target.parentElement.children[i].style.color = "black"
+                }
+            } catch {
+            }
+            
+        }
+
+        if(target.classList == "h1") {
+            target.style.backgroundColor = "rgb(19, 134, 241)";
+            target.style.color = "white";
+            target.style.boxShadow = "0px 0px 3px 3px rgb(12, 94, 246)"
+        }
+        
+    }
 }
 
 
